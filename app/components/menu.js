bb3aa68c95f973ca9fb11739c493d5eba4215b81
parika054 <131924696+parika054@users.noreply.github.com>
2024-04-21 20:14:16 -0600

    Update menu.js

--- app/components/menu.js
+++ app/components/menu.js
@@ -11,7 +11,7 @@ function Menu({ isOpen }) {
     <div className={`fixed inset-0 transition-opacity duration-300 ${isOpen ? "opacity-100 z-30" : "opacity-0 z-0"}`}>
       <div className={`absolute inset-0 transition-transform duration-300 bg-gray-900 bg-opacity-80 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
         <nav className="flex flex-col items-center justify-center h-full pt-20 space-y-8">
-         <MenuItem label="HOME" active={activePage === "home page"} onClick={() => handleActivePage("projects")} href="#nav" />
+         
           <MenuItem label="RESUME" href="/Technical_Resume_Parika.pdf" />
           <MenuItem label="CONTACT" active={activePage === "contact"} onClick={() => handleActivePage("contact")} href="mailto:parika054@gmail.com" />
           <MenuItem label="SKILLS" active={activePage === "skills"} onClick={() => handleActivePage("skills")} href="./components" />
